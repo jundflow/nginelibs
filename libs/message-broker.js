22c3b39b47eec61290fd77709480cec4f22cc720
Juniawan <juniawan@dflow.com.au>
2019-09-20 13:57:24 +0800

    Update push event broker

--- libs/message-broker.js
+++ libs/message-broker.js
@@ -8,28 +8,48 @@ class MsgBroker{
   }
 
   async pushEvent(channel, data, cb){
-    this.broker.createChannel().then(ch => {
-      const ok = ch.assertQueue(channel, { durable: false });
-      ok.then(() => {
-        ch.sendToQueue(channel, Buffer.from(JSON.stringify({data})));
-        
-        if(typeof cb !== "undefined"){
-          cb(null, ch.close());
-        }
-      }).catch(err => {
-        if(typeof cb !== "undefined"){
-          cb(err, null);
-        }else{
-          console.log(err);
-        }
-      })
+    this.broker.then(function(conn) {
+      return conn.createChannel();
+    }).then(function(ch) {
+      return ch.assertQueue(channel, { durable: false }).then(function(ok) {
+        return ch.sendToQueue(channel, Buffer.from(JSON.stringify({data})));
+      });
+    }).then(function(ch){
+      if(typeof cb !== "undefined"){
+        cb(null, ch.close());
+      }
     }).catch(err => {
       if(typeof cb !== "undefined"){
         cb(err, null);
       }else{
         console.log(err);
       }
-    })
+    });
+
+
+
+    // this.broker.createChannel().then(ch => {
+    //   const ok = ch.assertQueue(channel, { durable: false });
+    //   ok.then(() => {
+    //     ch.sendToQueue(channel, Buffer.from(JSON.stringify({data})));
+        
+    //     if(typeof cb !== "undefined"){
+    //       cb(null, ch.close());
+    //     }
+    //   }).catch(err => {
+    //     if(typeof cb !== "undefined"){
+    //       cb(err, null);
+    //     }else{
+    //       console.log(err);
+    //     }
+    //   })
+    // }).catch(err => {
+    //   if(typeof cb !== "undefined"){
+    //     cb(err, null);
+    //   }else{
+    //     console.log(err);
+    //   }
+    // })
   }
 
   async sendInfo(data, cb){
