2dbbbadfa60eaafbc812d6939040f53ba8ae172c
Juniawan <juniawan@dflow.com.au>
2019-09-24 10:34:45 +0800

    Fix index object

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  MessageBroker = require("./libs/message-broker"),
-  LogProfiler = require("./libs/log-profiler"),
-  JsonRenderer = require("./libs/json-renderer"),
-  MapToJson = require("./libs/map-to-json"),
-  StrRandom = require("./libs/str-random"),
-  StrSlugify = require("./libs/str-slugify"),
-  ConfigFetcher = require("./libs/config-fetcher")
+  MessageBroker: require("./libs/message-broker"),
+  LogProfiler: require("./libs/log-profiler"),
+  JsonRenderer: require("./libs/json-renderer"),
+  MapToJson: require("./libs/map-to-json"),
+  StrRandom: require("./libs/str-random"),
+  StrSlugify: require("./libs/str-slugify"),
+  ConfigFetcher: require("./libs/config-fetcher")
 }
